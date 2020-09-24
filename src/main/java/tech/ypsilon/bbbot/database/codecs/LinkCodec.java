75e72b5863a5ff450476be9ad568e2bb5247916b
Juliany <julian.leitner@hotmail.de>
2020-09-24 23:23:07 +0200

    LinkCodec decode and encode

--- src/main/java/tech/ypsilon/bbbot/database/codecs/LinkCodec.java
+++ src/main/java/tech/ypsilon/bbbot/database/codecs/LinkCodec.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import net.dv8tion.jda.api.entities.User;
 import org.bson.BsonReader;
+import org.bson.BsonType;
 import org.bson.BsonWriter;
 import org.bson.codecs.Codec;
 import org.bson.codecs.DecoderContext;
@@ -12,22 +13,32 @@ import org.bson.codecs.EncoderContext;
 import org.bson.types.ObjectId;
 import tech.ypsilon.bbbot.database.MongoController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class LinkCodec implements Codec<LinkCodec> {
 
-    public static final LinkCodec EMPTY_HOLDER = new LinkCodec(null, null, null, null);
+    public static final LinkCodec EMPTY_HOLDER = new LinkCodec(null, null, null, null, null);
 
     private final ObjectId _id;
+    private final String name;
     private final List<String> keywords;
     private final Long userId;
     private final String link;
 
-    public static LinkCodec createLink(User user, String link, List<String> keywords) {
-        LinkCodec linkCodec = new LinkCodec(new ObjectId(), keywords, user.getIdLong(), link);
+    public static LinkCodec createLink(String name, User user, String link, List<String> keywords) {
+        LinkCodec linkCodec = new LinkCodec(new ObjectId(), name, keywords, user.getIdLong(), link);
         getCollection().insertOne(linkCodec);
         return linkCodec;
+    }
+
+    public static FindIterable<LinkCodec> getAllLinks() {
+        return getCollection().find();
+    }
+
+    public static FindIterable<LinkCodec> getLinksForName(String regEx) {
+        return getCollection().find(Filters.regex("name", regEx));
     }
 
     public static FindIterable<LinkCodec> getLinksFromUser(User user) {
@@ -42,7 +53,8 @@ public class LinkCodec implements Codec<LinkCodec> {
         return getCollection().find(Filters.eq("_id", _id)).first();
     }
 
-    private LinkCodec(ObjectId _id, List<String> keywords, Long userId, String link) {
+    private LinkCodec(ObjectId _id, String name, List<String> keywords, Long userId, String link) {
+        this.name = name;
         this._id = _id;
         this.keywords = keywords;
         this.userId = userId;
@@ -69,14 +81,41 @@ public class LinkCodec implements Codec<LinkCodec> {
         return link;
     }
 
-    @Override
-    public LinkCodec decode(BsonReader bsonReader, DecoderContext decoderContext) {
-        return null;
+    public String getName() {
+        return name;
     }
 
     @Override
-    public void encode(BsonWriter bsonWriter, LinkCodec linkCodec, EncoderContext encoderContext) {
+    public LinkCodec decode(BsonReader reader, DecoderContext decoderContext) {
+        reader.readStartDocument();
+        ObjectId _id = reader.readObjectId("_id");
+        String name = reader.readString("name");
 
+        reader.readName("keywords");
+        List<String> keywords = new ArrayList<>();
+        while(reader.readBsonType() != BsonType.END_OF_DOCUMENT)
+            keywords.add(reader.readString());
+
+        Long userId = reader.readInt64("userId");
+        String link = reader.readString("link");
+
+        reader.readEndDocument();
+        return new LinkCodec(_id, name, keywords, userId, link);
+    }
+
+    @Override
+    public void encode(BsonWriter writer, LinkCodec data, EncoderContext encoderContext) {
+        writer.writeStartDocument();
+
+        writer.writeObjectId("_id", data._id);
+        writer.writeString("name", data.name);
+        writer.writeStartArray("keywords");
+        for (String keyword : data.keywords)
+            writer.writeString(keyword);
+        writer.writeInt64("userId", data.userId);
+        writer.writeString("link", data.link);
+
+        writer.writeEndDocument();
     }
 
     @Override
