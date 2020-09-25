92d68f4c104d6d47d42127a8276df3395e9b347d
Niklas @yDev <niklas@magic-computer.de>
2020-09-25 12:45:10 +0200

    Include db connection for AddDirectoryCommand

--- src/main/java/tech/ypsilon/bbbot/discord/command/AddDirectoryCommand.java
+++ src/main/java/tech/ypsilon/bbbot/discord/command/AddDirectoryCommand.java
@@ -3,6 +3,7 @@ package tech.ypsilon.bbbot.discord.command;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
+import tech.ypsilon.bbbot.database.codecs.DirectoryCodec;
 import tech.ypsilon.bbbot.database.codecs.LinkCodec;
 import tech.ypsilon.bbbot.util.EmbedUtil;
 
@@ -27,15 +28,22 @@ public class AddDirectoryCommand extends Command implements PrivateChat {
 
     @Override
     public void onPrivateExecute(PrivateMessageReceivedEvent e, String[] args) {
-        // bb mkdir [name] {names...}
-        if(args.length == 0) {
+        // bb mkdir [name] [shared] {names...}
+        if(args.length < 2) {
             EmbedBuilder b = EmbedUtil.createErrorEmbed();
-            b.setDescription("Übergebe den Namen des zu erstellenden Ordners");
+            b.setDescription("Übergebe den Namen und die Sichtbarkeit (public/private) des zu erstellenden Ordners");
             e.getAuthor().openPrivateChannel().flatMap(privateChannel -> privateChannel.sendMessage(b.build())).queue();
             return;
         }
 
-        // todo: [name] schon vorhanden?
+        if (!args[1].equals("public") && !args[1].equals("private")) {
+            EmbedBuilder b = EmbedUtil.createErrorEmbed();
+            b.setDescription("Übergebe entweder public oder private nach dem Namen");
+            e.getAuthor().openPrivateChannel().flatMap(privateChannel -> privateChannel.sendMessage(b.build())).queue();
+            return;
+        }
+
+        boolean shared = args[1].equals("public");
 
         List<LinkCodec> compatibleNames = new ArrayList<>();
         List<String> incompatibleNames = new ArrayList<>();
@@ -48,8 +56,18 @@ public class AddDirectoryCommand extends Command implements PrivateChat {
             if (!found) incompatibleNames.add(args[i]);
         }
 
+        DirectoryCodec directoryCodec = DirectoryCodec.addDirectory(e.getAuthor(), args[0], compatibleNames);
+        if (directoryCodec == null) {
+            EmbedBuilder b = EmbedUtil.createErrorEmbed();
+            b.setDescription("Eine Sammlung mit diesem Namen existiert bereits");
+            e.getAuthor().openPrivateChannel().flatMap(privateChannel -> privateChannel.sendMessage(b.build())).queue();
+            return;
+        }
+
+        directoryCodec.setShared(shared);
+
         EmbedBuilder b = EmbedUtil.createSuccessEmbed();
-        if (args.length > 1) {
+        if (args.length > 2) {
             b.setDescription("Deine Sammlung wurde erfolgreich erstellt. Insgesamt wurden " + compatibleNames.size() +
                     " gefunden und hinzugefügt. Nicht gefunden: " + incompatibleNames.size() + ". Hinzugefügt: ");
             for (LinkCodec compatibleName : compatibleNames) {
