de6500a5817ddabf4bfcdaf48a044a9cbdf5550f
Gregor <gregor.czubayko@gmail.com>
2020-09-25 18:17:45 +0200

    Update EmbedUtil

--- src/main/java/tech/ypsilon/bbbot/util/EmbedUtil.java
+++ src/main/java/tech/ypsilon/bbbot/util/EmbedUtil.java
@@ -5,48 +5,53 @@ import java.awt.*;
 
 public class EmbedUtil {
 
-    public static EmbedBuilder createErrorEmbed() {
+    public static EmbedBuilder createDefaultEmbed() {
         EmbedBuilder embedBuilder = new EmbedBuilder();
-        embedBuilder.setTitle("Fehler");
-        embedBuilder.setColor(Color.decode("#FF7770"));
+        embedBuilder.setColor(Color.decode("#1a4064"));
         embedBuilder.setFooter("Elite-Nachricht",
                 "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Logo_KIT.svg/2000px-Logo_KIT.svg.png");
         return embedBuilder;
     }
 
+    public static EmbedBuilder createErrorEmbed() {
+        EmbedBuilder embedBuilder = createDefaultEmbed();
+        embedBuilder.setTitle("Fehler");
+        embedBuilder.setColor(Color.decode("#FF7770"));
+        return embedBuilder;
+    }
+
     public static EmbedBuilder createSuccessEmbed() {
-        EmbedBuilder embedBuilder = new EmbedBuilder();
+        EmbedBuilder embedBuilder = createDefaultEmbed();
         embedBuilder.setTitle("Erfolg");
         embedBuilder.setColor(Color.decode("#97DBA2"));
-        embedBuilder.setFooter("Elite-Nachricht",
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Logo_KIT.svg/2000px-Logo_KIT.svg.png");
         return embedBuilder;
     }
 
     public static EmbedBuilder createListEmbed(boolean hasMatches) {
         if (!hasMatches) {
-            EmbedBuilder embedBuilder = new EmbedBuilder();
+            EmbedBuilder embedBuilder = createDefaultEmbed();
             embedBuilder.setTitle("Keine Ergebnisse");
             embedBuilder.setColor(Color.decode("#FF7770"));
-            embedBuilder.setFooter("Elite-Nachricht",
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Logo_KIT.svg/2000px-Logo_KIT.svg.png");
             return embedBuilder;
         } else {
-            EmbedBuilder embedBuilder = new EmbedBuilder();
+            EmbedBuilder embedBuilder = createDefaultEmbed();
             embedBuilder.setTitle("Ergebnisliste");
             embedBuilder.setColor(Color.decode("#97DBA2"));
-            embedBuilder.setFooter("Elite-Nachricht",
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Logo_KIT.svg/2000px-Logo_KIT.svg.png");
             return embedBuilder;
         }
     }
 
     public static EmbedBuilder createDirectoryEmbed() {
-        EmbedBuilder embedBuilder = new EmbedBuilder();
+        EmbedBuilder embedBuilder = createDefaultEmbed();
         embedBuilder.setTitle("Dein Directory");
         embedBuilder.setColor(Color.decode("#6470FA"));
-        embedBuilder.setFooter("Elite-Nachricht",
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Logo_KIT.svg/2000px-Logo_KIT.svg.png");
+        return embedBuilder;
+    }
+
+    public static EmbedBuilder createInfoEmbed() {
+        EmbedBuilder embedBuilder = createDefaultEmbed();
+        embedBuilder.setTitle("Info");
+        embedBuilder.setColor(Color.decode("#41e3fc"));
         return embedBuilder;
     }
 
