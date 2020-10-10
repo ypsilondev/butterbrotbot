4dde4df467c76b82bbe05ca696beb436698d2567
yJulian <yJulian@outlook.de>
2020-10-10 22:46:04 +0200

    Increase version to 1.2.5 (Same as Docker-Image)

--- src/main/java/tech/ypsilon/bbbot/BotInfo.java
+++ src/main/java/tech/ypsilon/bbbot/BotInfo.java
@@ -6,7 +6,7 @@ public interface BotInfo {
 
     String VERSION_MAJOR = "1";
     String VERSION_MINOR = "2";
-    String VERSION_REVISION = "0";
+    String VERSION_REVISION = "5";
     String VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_REVISION;
 
 }
